f88bba4c635e77eb57dc29b9a31fce1c4ec0cc78
Luana Carvalho <lcarvalhodev10@gmail.com>
2026-03-05 19:20:20 +0100

    Ejercicio extra - Parking

--- src/_collections/Parking.java
+++ src/_collections/Parking.java
@@ -8,7 +8,9 @@ import java.util.Random;
 import java.util.Set;
 
 /***
- * Responsable de gestionar entradas y salidas de coches de parking a través de listas y mapas.
+ * Responsable de gestionar entradas y salidas de coches de parking a través de
+ * listas y mapas.
+ * 
  * @author luana
  * @version 1.0
  */
@@ -26,15 +28,26 @@ public class Parking {
 	private Map<Coche, Integer> contadorCochesIguales;
 	// Set de tipos de coches
 	private Set<Coche> setCoches;
+	// Contador de Plazas totales
+	private static Map<Parking, Integer> mapaParkings = new HashMap<>();
+	// Contador de Plazas ocupadas
+	private static Map<Parking, Integer> contadorPlazasOcupadas = new HashMap<>();
+	// Número de parkings
+	private static int numeroParkings = 0;
 
 	public Parking(String nombre, int totalPlazas) {
 		this.nombre = nombre;
 		this.totalPlazas = totalPlazas;
-		
+
 		listaCoches = new ArrayList<Coche>();
 		contadorColor = new HashMap<>();
 		contadorMarca = new HashMap<>();
 		contadorCochesIguales = new HashMap<>();
+
+		mapaParkings.put(this, this.totalPlazas);
+		contadorPlazasOcupadas.put(this, 0);
+		
+		numeroParkings++;
 	}
 
 	/***
@@ -55,26 +68,29 @@ public class Parking {
 		}
 		// si no hay plazas y ya no estaba dentro se puede entrar
 		boolean puedeEntrar = hayPlazas && !estaDentro;
+
 		
-		
-		// adiciona a lista
 		if (puedeEntrar) {
+			// adiciona a lista
 			listaCoches.add(c);
+			
+			// actualiza mapa de plazas ocupadas
+			contadorPlazasOcupadas.put(this, contadorPlazasOcupadas.get(this) + 1);
+
+			// actualiza mapa de colores
+			Color color = c.getColor();
+			Integer elementoColor = contadorColor.get(color);
+			contadorColor.put(color, elementoColor == null ? 1 : elementoColor + 1);
+
+			// actualiza mapa de marcas
+			Marca marca = c.getMarca();
+			Integer elementoMarca = contadorMarca.get(marca);
+			contadorMarca.put(marca, elementoMarca == null ? 1 : elementoMarca + 1);
+
+			// actualiza mapa de coches iguales
+			Integer elementoCochesIguales = contadorCochesIguales.get(c);
+			contadorCochesIguales.put(c, elementoCochesIguales == null ? 1 : elementoCochesIguales + 1);
 		}
-		
-		// actualiza mapa de colores
-		Color color = c.getColor();
-		Integer elementoColor = contadorColor.get(color);
-		contadorColor.put(color, elementoColor == null ? 1 : elementoColor +1);
-
-		// actualiza mapa de marcas
-		Marca marca = c.getMarca();
-		Integer elementoMarca = contadorMarca.get(marca);
-		contadorMarca.put(marca, elementoMarca == null ? 1 : elementoMarca + 1);
-
-		// actualiza mapa de coches iguales
-		Integer elementoCochesIguales = contadorCochesIguales.get(c);
-		contadorCochesIguales.put(c, elementoCochesIguales == null ? 1 : elementoCochesIguales +1);
 
 		return puedeEntrar;
 	}
@@ -87,14 +103,15 @@ public class Parking {
 	 */
 	public boolean saleCoche(Coche c) {
 		boolean estaVacio = listaCoches.isEmpty();
-		boolean estaDentro = false; 
+		boolean estaDentro = false;
 		for (Coche coche : listaCoches) {
-			if(coche == c) {
-				estaDentro = true; 
+			if (coche == c) {
+				estaDentro = true;
 			}
 		}
-		if (!estaDentro|| estaVacio) return false;
-		
+		if (!estaDentro || estaVacio)
+			return false;
+
 		// remove de lista
 		listaCoches.remove(c);
 
@@ -123,6 +140,9 @@ public class Parking {
 		} else {
 			contadorCochesIguales.put(c, elementoCocheIgual - 1);
 		}
+
+		// actualiza mapa de plazas ocupadas
+		contadorPlazasOcupadas.put(this, contadorPlazasOcupadas.get(this) + 1);
 
 		return estaDentro;
 	}
@@ -159,6 +179,58 @@ public class Parking {
 	}
 
 	/***
+	 * Translada coches de un parking a otro
+	 * 
+	 * @param p Parking al que se translada coches de parking seleccionado
+	 * @return true si se puede transladar
+	 */
+	public boolean transladarParking(Parking p) {
+		// verificar si hay sitio suficiente en parking p y si parking llamado no está
+		// vacio
+		boolean haySitioEnP = p.totalPlazas - p.listaCoches.size() >= listaCoches.size();
+		boolean estaVacioParkAntes = listaCoches.isEmpty();
+		boolean sePuedeTransladar = haySitioEnP && !estaVacioParkAntes;
+		// añade listado en parking p y lo remove de parking anterior
+		if (!sePuedeTransladar)
+			return false;
+		// copia para no tocar la original mientras iteramos
+		List<Coche> copia = new ArrayList<>(listaCoches);
+		for (Coche coche : copia) {
+			p.entraCoche(coche);
+			saleCoche(coche);
+		}
+		return true;
+	}
+
+	/***
+	 * Mueve un coche de un parking a otro
+	 * 
+	 * @param c coche que se translada
+	 * @param p Parking al que se translada coches de parking seleccionado
+	 * @return
+	 */
+	public boolean moverCoche(Coche c, Parking p) {
+		// verifica si hay plaza en parking p para añadir nuevo coche
+		boolean haySitioEnP = p.totalPlazas - p.listaCoches.size() >= 1;
+		// verifica si coche c esta dentro de parking anterior
+		boolean estaDentro = false;
+		for (Coche coche : listaCoches) {
+			if (coche == c) {
+				estaDentro = true;
+				break;
+			}
+		}
+		boolean puedeMoverCoche = haySitioEnP && estaDentro;
+		// si no pasa por verificación retorna false;
+		if (!puedeMoverCoche)
+			return false;
+		// añade coche a nueva lista, remove de lista anterior
+		p.entraCoche(c);
+		saleCoche(c);
+		return true;
+	}
+
+	/***
 	 * Información de listado de coches en parking.
 	 */
 	public void reportParking() {
@@ -178,7 +250,7 @@ public class Parking {
 	 */
 	public void reportColores() {
 		System.out.println("LISTADO DE COLORES");
-		System.out.println("--------------");
+		System.out.println("------------------");
 		System.out.println("Parking: " + nombre);
 
 		for (Map.Entry<Color, Integer> entry : contadorColor.entrySet()) {
@@ -197,7 +269,7 @@ public class Parking {
 	 */
 	public void reportMarcas() {
 		System.out.println("LISTADO DE MARCAS");
-		System.out.println("--------------");
+		System.out.println("-----------------");
 		System.out.println("Parking: " + nombre);
 		for (Map.Entry<Marca, Integer> entry : contadorMarca.entrySet()) {
 			Marca marca = entry.getKey();
@@ -212,7 +284,7 @@ public class Parking {
 	 */
 	public void reportCochesIguales() {
 		System.out.println("LISTADO DE COCHES IGUALES");
-		System.out.println("--------------");
+		System.out.println("-------------------------");
 		System.out.println("Parking: " + nombre);
 
 		for (Map.Entry<Coche, Integer> entry : contadorCochesIguales.entrySet()) {
@@ -230,7 +302,7 @@ public class Parking {
 	 */
 	public void reportSetCoches() {
 		System.out.println("LISTADO SET DE COCHES");
-		System.out.println("--------------");
+		System.out.println("---------------------");
 		System.out.println("Parking: " + nombre);
 		setCoches = contadorCochesIguales.keySet();
 
@@ -238,7 +310,36 @@ public class Parking {
 			System.out.printf("Coche: %s %s%n", coche.getMarca().name(), coche.getColor().name());
 		}
 
-		System.out.printf("Total coches: %d.%n", setCoches.size());
+		System.out.println("Total coches: " + setCoches.size());
+	}
+
+	public void reportMapaParkings() {
+		System.out.println("LISTADO PARKINGS");
+		System.out.println("-------------------------------");
+
+		for (Map.Entry<Parking, Integer> entry : mapaParkings.entrySet()) {
+			Parking park = entry.getKey();
+			Integer totalPlazas = entry.getValue();
+			System.out.printf("Parking: %s - plazas totales: %d.%n", park.nombre, totalPlazas);
+		}
+
+		System.out.println("Total de parkings: " + mapaParkings.size() + ".");
+	}
+
+	public void reportContadorPlazasOcupadas() {
+		System.out.println("LISTADO PLAZAS OCUPADAS PARKINGS");
+		System.out.println("--------------------------------");
+		
+		for (Map.Entry<Parking, Integer> entry : contadorPlazasOcupadas.entrySet()) {
+			Parking park = entry.getKey();
+			Integer plazaOcupadas = entry.getValue();
+			if(!(plazaOcupadas == 0)) {
+				System.out.printf("Parking: %s tiene %d plazas ocupadas.%n", park.nombre, plazaOcupadas);
+			}
+			
+		}
+
+		System.out.println("Total de parkings: " + mapaParkings.size() + ".");
 	}
 
 }
--- src/_collections/Test.java
+++ src/_collections/Test.java
@@ -7,33 +7,51 @@ public class Test {
 	public static void main(String[] args) {
 		Coche c1 = new Coche(Marca.BM, Color.AM); 
 		Coche c2 = new Coche(Marca.BM, Color.AM); 
-		Parking p = new Parking("Puerta Jerez", 20);
-		
-		System.out.println(p.entraCoche(c2));
-	
-		System.out.println(p.entraCoche(c1));
+		Parking p0 = new Parking("Puerta Jerez", 20);
+		Parking p1 = new Parking("Alameda", 25);
+		Parking p2 = new Parking("Atlantic", 30);
+
+		System.out.println(p0.entraCoche(c2));
+		System.out.println(p0.entraCoche(c1));
 		for (int i = 0; i < 15; i++) {
-			p.entraCoche(new Coche());			
+			p0.entraCoche(new Coche());	
+			p2.entraCoche(new Coche());
 		}
 		
-		p.reportParking();
+		/*p.reportParking();
 		p.reportColores();
 		p.reportMarcas();
 		p.reportCochesIguales();
-		p.reportSetCoches();
+		p.reportSetCoches();*/
 		
-		p.saleCocheAleatorio();
-		System.out.println(p.saleCoche(c2));
-		System.out.println(p.saleCoche(c1));
-		p.vaciarParking();
-		
-		p.reportParking();
-		p.reportColores();
-		p.reportMarcas();
-		p.reportCochesIguales();
-		p.reportSetCoches();
+		//p.saleCocheAleatorio();
+		//System.out.println(p.saleCoche(c2));
+		//System.out.println(p.saleCoche(c1));
+		//p.vaciarParking();
 		
 		
+		p0.reportParking();
+		p1.reportParking();
+		p0.reportColores();
+		p1.reportColores();
+		System.out.println(p0.transladarParking(p1));
+		System.out.println("-----------------------");
+		p0.reportParking();
+		p1.reportParking();
+		p0.reportColores();
+		p1.reportColores();
+		
+		
+		System.out.println(p0.moverCoche(c2, p2));
+		System.out.println(p0.moverCoche(c1, p2));
+		p0.reportParking();
+		p2.reportParking();
+		p0.reportColores();
+		p2.reportColores();
+		
+		p0.reportMapaParkings();
+		p0.reportContadorPlazasOcupadas();
+			
 	}
 
 }
